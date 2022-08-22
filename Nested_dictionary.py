17d6323fe6fd2cc7e6237f025cdac7844257272d
sunita sharma <111037179+sunitashrma@users.noreply.github.com>
2022-08-22 21:45:14 +0530

    Commit new files here

---
+++ Nested_dictionary.py
@@ -0,0 +1,17 @@
+
+#Nested Dictionary
+score = { }
+score ["Test 1"] = { }
+score [ "Test 2"] = { }
+
+score ["Test 1"] ["Dhawan"] =84
+score ["Test 1" ] ["Kohli" ] = 200
+score ["Test 2"]  ["Dhawan"] = 27
+
+
+
+print (score)
+KEYS = score.keys ()
+print (KEYS)
+
+
