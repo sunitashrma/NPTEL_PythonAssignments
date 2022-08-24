58e02dd0a1179512c7d324726419946aa4dda36c
sunita sharma <111037179+sunitashrma@users.noreply.github.com>
2022-08-24 22:03:42 +0530

    commit tuple file

---
+++ sum_of_tuple_elements.py
@@ -0,0 +1,19 @@
+
+# Tuple summation
+# initializing tup
+
+test_tuple = (7, 8, 9, 1, 10, 7)
+
+# printing original tuple
+
+print (" The original tuple is :" + str(test_tuple))
+
+# using list () + sum ()
+
+res = sum(list(test_tuple))
+
+# printing result
+
+print("The summation of tuple elements are : " + str(res))
+
+
