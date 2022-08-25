80de10c135dab0b341ed0229fb330644cfb3e12c
sunita sharma <111037179+sunitashrma@users.noreply.github.com>
2022-08-25 22:55:08 +0530

    Commit a change in file

--- Nested_dictionary.py
+++ Nested_dictionary.py
@@ -3,10 +3,12 @@
 score = { }
 score ["Test 1"] = { }
 score [ "Test 2"] = { }
+score ["Test 3"] = { }
 
 score ["Test 1"] ["Dhawan"] =84
 score ["Test 1" ] ["Kohli" ] = 200
 score ["Test 2"]  ["Dhawan"] = 27
+score ["Test 3"]  ["Dhawan"] = 78
 
 
 
