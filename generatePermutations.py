52182d6f3c4f8b20033c95a51081039785e25376
sunita sharma <111037179+sunitashrma@users.noreply.github.com>
2022-08-28 10:19:03 +0530

    Commit File

---
+++ generatePermutations.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+
+l= list (permutations(range(1,4)))
+
+print (l)
+
+# Permutation:
+
+from itertools import permutations
+list_perm = list(permutations([1,2,3,4],2))
+
+print (list_perm)
+
