c7652b00d21031f407886cbc830bde7dfd062d36
sunita sharma <111037179+sunitashrma@users.noreply.github.com>
2022-09-14 12:01:40 +0530

    Commit infinte loops file

---
+++ infiniteLoops_python.py
@@ -0,0 +1,21 @@
+# Examples of Infinte loops in python
+
+
+i = 0
+while i <=10:
+    print("I will run forever")
+
+
+value = True
+while (value):
+
+   print("I am infinite loop")
+
+
+
+
+f = 196
+x=  16
+while f>=x^2:
+   print (" I will run infinitely")
+   
